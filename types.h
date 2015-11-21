dea8d42d147f4eda4f3c5e7ee9237732e352f0bf
Matt Hansen <mhansen@cfl.rr.com>
2015-11-21 17:11:09 -0500

    Added typedh for cross platform support

--- types.h
+++ types.h
@@ -1,5 +1,11 @@
-typedef float_t float;
-
+typedef float float_t;
+typedef long int32_t;
+typedef short int16_t;
+typedef char char8_t;
+typedef unsigned char uchar8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+typedef bool bool8_t;
 
 typedef struct 
 {
